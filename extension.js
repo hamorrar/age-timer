2f1d146ab07e074a9ff8a78bbce79e85a9e209a9
Hilal Morrar <hamorrar@ucsc.edu>
2021-09-14 22:37:11 -0700

    finished

--- extension.js
+++ extension.js
@@ -1,34 +1,38 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
 
-/* exported init */
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const GLib = imports.gi.GLib;
+const Mainloop = imports.mainloop;
 
-class Extension {
-    constructor() {
-    }
+let panelAgeText, panelButton, timeout;
 
-    enable() {
-    }
-
-    disable() {
-    }
-}
+let counter = 0;
 
 function init() {
-    return new Extension();
+    panelButton = new St.Bin({
+        style_class: "panel-button"
+    });
+    
+    panelAgeText = new St.Label({ text: "Initializing", style_class: "panelAgeText", y_align: Clutter.ActorAlign.CENTER });
+    panelButton.set_child(panelAgeText);
+}
+
+function enable() {
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+    timeout = Mainloop.timeout_add(1, calculateAge);
+}
+
+function calculateAge() {
+    let bday = new Date('2000-05-20T00:15:00');
+    let now = new Date();
+    panelAgeText.set_text(((now-bday)/31556952000).toFixed(9).toString());
+    return true;
+}
+
+function disable() {
+    Mainloop.source_remove(timeout);
+    Main.panel._rightBox.remove_child(button);
 }
